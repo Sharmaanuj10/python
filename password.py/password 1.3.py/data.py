4d6c8e240d57d0adac9ec062afcd61c044f38831
Anuj Sharma <71202190+Sharmaanuj10@users.noreply.github.com>
2021-07-10 16:46:13 +0530

    Update data.py

--- password.py/password 1.3.py/data.py
+++ password.py/password 1.3.py/data.py
@@ -1,3 +1,2 @@
-password = []
-password.append(input(" Enter the name : "))
-print(password)
+passwords = ['use' , 'lol'] #you can add n number of string here or password and username below
+usernames = ['Anuj Sharma' , 'Anuj','']
