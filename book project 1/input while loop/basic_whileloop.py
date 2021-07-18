a49137ef307793316f47db34092df7c1ab6bf591
Anuj Sharma <71202190+Sharmaanuj10@users.noreply.github.com>
2021-07-18 12:51:11 +0530

    Delete book project 1 directory

--- book project 1/input while loop/basic_whileloop.py
+++
@@ -1,9 +0,0 @@
-number = 2
-
-# while loop run till the block is true
-while number <=8:
-    
-    print(number)
-
-    number+= 1   # <---- number= number+1 for increment and stoping to create infinte loop
-
